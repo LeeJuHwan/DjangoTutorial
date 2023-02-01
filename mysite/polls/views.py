c8fa7f08a5331dbd161f69ad4ba5e554452be9d4
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2023-02-01 12:30:06 +0900

    STEP 3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,4 +2,14 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, My first App.")
+    return HttpResponse("polls index")
+
+def detail(request, question_id) :
+    return HttpResponse(f"looking at question_id page {question_id}.")
+
+def results(request, question_id) :
+    response = "Yoe're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id) :
+    return HttpResponse(f"looking at voting at question page {question_id}.")
